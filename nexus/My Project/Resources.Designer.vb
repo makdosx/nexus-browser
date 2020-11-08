7a03b89c3d6be2ebbb5f266d978637daf0d9e9ec
mak <makindosx@gmail.com>
2020-11-08 21:27:58 +0200

    Add files via upload

--- nexus/My Project/Resources.Designer.vb
+++ nexus/My Project/Resources.Designer.vb
@@ -274,6 +274,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property metapedia() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("metapedia", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property microsoft() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("microsoft", resourceCulture)
