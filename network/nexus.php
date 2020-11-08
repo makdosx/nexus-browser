1f3f5eba32dd8ff1db09dda8c71f18718cff16d4
mak <makindosx@gmail.com>
2020-11-08 21:57:07 +0200

    Create nexus.php

---
+++ network/nexus.php
@@ -0,0 +1,147 @@
+<html>
+
+<head>
+
+
+   <meta charset="utf-8">
+  <title> Nexus Network </title>
+
+  <link rel="icon" type="image/png" href="http://nexbrowser.c1.biz/imgs/nexus.ico"/>
+
+<style>
+body
+{
+background-color: #341818;	
+color: white;
+}
+
+a 
+{
+
+color: red;
+}
+
+
+</style>
+
+
+</head>
+
+<body>
+
+
+<?php
+
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+error_reporting(E_ALL);
+
+		 
+	
+function getBrowser() { 
+  $u_agent = $_SERVER['HTTP_USER_AGENT'];
+  $bname = 'Unknown';
+  $platform = 'Unknown';
+  $version= "";
+
+  //First get the platform?
+  if (preg_match('/linux/i', $u_agent)) {
+    $platform = 'linux';
+  }elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+    $platform = 'mac';
+  }elseif (preg_match('/windows|win32/i', $u_agent)) {
+    $platform = 'windows';
+  }
+
+  // Next get the name of the useragent yes seperately and for good reason
+  if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)){
+    $bname = 'Internet Explorer';
+    $ub = "MSIE";
+  }elseif(preg_match('/Firefox/i',$u_agent)){
+    $bname = 'Mozilla Firefox';
+    $ub = "Firefox";
+  }elseif(preg_match('/OPR/i',$u_agent)){
+    $bname = 'Opera';
+    $ub = "Opera";
+  }elseif(preg_match('/Chrome/i',$u_agent) && !preg_match('/Edge/i',$u_agent)){
+    $bname = 'Google Chrome';
+    $ub = "Chrome";
+  }elseif(preg_match('/Safari/i',$u_agent) && !preg_match('/Edge/i',$u_agent)){
+    $bname = 'Apple Safari';
+    $ub = "Safari";
+  }elseif(preg_match('/Netscape/i',$u_agent)){
+    $bname = 'Netscape';
+    $ub = "Netscape";
+  }elseif(preg_match('/Edge/i',$u_agent)){
+    $bname = 'Edge';
+    $ub = "Edge";
+  }elseif(preg_match('/Trident/i',$u_agent)){
+    $bname = 'Internet Explorer';
+    $ub = "MSIE";
+  }
+
+  // finally get the correct version number
+  $known = array('Version', $ub, 'other');
+  $pattern = '#(?<browser>' . join('|', $known) .
+')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+  if (!preg_match_all($pattern, $u_agent, $matches)) {
+    // we have no matching number just continue
+  }
+  // see how many we have
+  $i = count($matches['browser']);
+  if ($i != 1) {
+    //we will have two since we are not using 'other' argument yet
+    //see if version is before or after the name
+    if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+        $version= $matches['version'][0];
+    }else {
+        $version= $matches['version'][1];
+    }
+  }else {
+    $version= $matches['version'][0];
+  }
+
+  // check if we have a number
+  if ($version==null || $version=="") {$version="?";}
+
+  return array(
+    'userAgent' => $u_agent,
+    'name'      => $bname,
+    'version'   => $version,
+    'platform'  => $platform,
+    'pattern'    => $pattern
+  );
+} 
+
+// now try it
+$ua=getBrowser();
+$user_browser =  $ua['userAgent'];
+
+
+$nexus_browser = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.2; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)";
+
+    if ($user_browser == $nexus_browser)
+     {
+      }
+
+
+    else
+     {
+
+	 echo "<div align='center'> 
+	        <h1> <font color='green'> Nexus Network! </font> <font color='red'> Hide Content! </font> </h2>
+			    <hr> <br>
+	          <span style='font-size:200px;'> &#127760; &#128073; &#10060;</span>
+			    <br><br><br>
+	         <h2> <i> Get the <a href='https://github.com/makdosx/nexus-browser'> Nexus Browser </a> 
+			 for view this content! </i> </h2> 
+	       </div>";	   
+	 exit;	  
+	  }		
+		
+
+?>
+
+
+</body>
+</html>
